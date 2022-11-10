fc78322e718b2e8f5da6404423539228e98fd655
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-11-09 21:37:38 -0300

    feat: create courses list

--- src/index.js
+++ src/index.js
@@ -2,12 +2,27 @@ const express = require("express");
 
 const app = express();
 
+const courses = [
+  {
+    id: 1,
+    name: "NodeJS",
+  },
+  {
+    id: 2,
+    name: "ReactJS",
+  },
+  {
+    id: 3,
+    name: "React Native",
+  },
+];
+
 app.get("/", function (request, response) {
   return response.send("Tudo OK!");
 });
 
 app.get("/courses", (request, response) => {
-  return response.json(["Curso 1, Curso 2, Curso 3"]);
+  return response.json(courses);
 });
 
 app.listen(3333);
