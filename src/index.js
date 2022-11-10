72e771fd9c9b4e7a83116d958e60cd616c5a3fde
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-11-09 21:35:00 -0300

    feat: add route to list courses

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (request, response) {
-  response.send("Tudo OK!");
+  return response.send("Tudo OK!");
+});
+
+app.get("/courses", (request, response) => {
+  return response.json(["Curso 1, Curso 2, Curso 3"]);
 });
 
 app.listen(3333);
